229812599c517118e3854bc928d4e6615f2b70ce
viktorialoza <loza.viktoria2511@gmail.com>
2024-02-27 20:58:24 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 7;
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        int c = 54;
+        System.out.println(c/10+c%10);
+
+        double f = 2.5;
+        System.out.println((int)(f+0.5));
+
+    }
+}
